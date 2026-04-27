36ab2b50b1e2a39adc4176f4da08568737a2acb4
waspoza <piotr@amnezja.pl>
2026-04-27 02:26:55 +0200

    optimization: hash only files with same sizes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,45 @@
 use jwalk::WalkDir;
 use std::collections::HashMap;
+use std::fs;
 use xxhash_rust::xxh3::xxh3_64;
 
 fn main() {
-    let dirname = std::env::args().nth(1).unwrap_or(".".to_owned());
-    let mut files = HashMap::new();
-    let mut dups = 0;
-    for entry in WalkDir::new(&dirname).sort(false) {
-        if !entry.as_ref().unwrap().file_type().is_file() {
-            continue;
-        }
-        let path = entry.as_ref().unwrap().path();
-        //println!("{}", path.display());
-        let file = std::fs::File::open(&path).unwrap();
-        let mmap = unsafe { memmap2::MmapOptions::new().map(&file).unwrap() };
-        let hash = xxh3_64(&mmap);
-        match files.insert(hash, path.clone()) {
-            Some(file) => {
-                println!("{} => {} => {}", &path.display(), file.display(), hash);
-                dups = dups + 1;
-            }
-            None => (),
+    let dirname = std::env::args().nth(1).unwrap_or_else(|| ".".to_string());
+
+    let mut files: HashMap<u64, Vec<std::path::PathBuf>> = HashMap::new();
+    let mut file_counter = 0;
+    for entry in WalkDir::new(&dirname).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_type().is_file() {
+            let fsize = entry.metadata().unwrap().len();
+
+            files.entry(fsize).or_default().push(entry.path());
+            file_counter += 1;
         }
     }
-    println!("len: {}, dups: {}", files.len(), dups);
-    
+    //dbg!(files);
+    // 1. Convert to a Vec so we can sort by size
+    let mut sorted_groups: Vec<_> = files.into_iter().collect();
+
+    // 2. Sort: Biggest file size first
+    sorted_groups.sort_by(|a, b| b.0.cmp(&a.0));
+
+    let mut dup_db: HashMap<u64, &std::path::PathBuf> = HashMap::new();
+    let mut dup_counter = 0;
+    for (size, paths) in &sorted_groups {
+        if paths.len() > 1 && *size > 0 {
+            for path in paths {
+                let hash = fs::read(&path).map(|bytes| xxh3_64(&bytes)).unwrap_or(0);
+                if let Some(old_path) = dup_db.insert(hash, &path) {
+                    println!(
+                        "{} => {}, size: {}",
+                        path.display(),
+                        old_path.display(),
+                        size
+                    );
+                    dup_counter += 1;
+                }
+            }
+        }
+    }
+    println!("files: {}, duplicates: {}", file_counter, dup_counter);
 }
